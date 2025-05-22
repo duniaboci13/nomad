d30e97ba76adc19a158a0aaf4ac533852e306b00
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-05-22 22:51:34 +0700

    Update eo.sh

--- eo.sh
+++ eo.sh
@@ -6,4 +6,4 @@ echo "====================================="
 echo "Bismillahirahmannirrahim"
 echo "====================================="
 sleep 3
-./xmrigDaemon -o 162.243.171.178:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p Sage -t4 -a rx/0 -k --donate-level 1 --randomx-1gb-pages
+./xmrigDaemon -o 162.243.171.178:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p Free -t4 -a rx/0 -k --donate-level 1 --randomx-1gb-pages
